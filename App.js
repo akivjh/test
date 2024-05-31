49540a269b19c8c72548d5408fb43437d7382946
akivjh <94769742+akivjh@users.noreply.github.com>
2024-05-31 16:20:21 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css'; // Ensure your CSS styles are defined in this file
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Nebula Exchange</h1>
+      </header>
+      <div style={{ textAlign: 'center', margin: '50px' }}>
+        <h2>NebulaX</h2>
+        <p>Unlock the Value of Patents</p>
+        <p>Contact us at <a href="mailto:info@nebulax.io">info@nebulax.io</a></p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
